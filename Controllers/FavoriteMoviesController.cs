088316b2733baada61501640a4338d8db0b26368
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-10-27 22:28:29 +0700

    Создал заголовки запросов в контроллере

---
+++ Controllers/FavoriteMoviesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieCatalogAPI.Models;
+
+namespace MovieCatalogAPI.Controllers
+{
+    [Route("api/favorites")]
+    [ApiController]
+    public class FavoriteMoviesController : ControllerBase
+    {
+        [HttpGet, Authorize]
+        public async Task<MoviesList> Get()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost("{id}/add"), Authorize]
+        public async Task<IActionResult> AddMovie()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}/delete"), Authorize]
+        public async Task<IActionResult> DeleteMovie()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

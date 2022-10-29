7627236b9e3b4a1017022011ca379c02826074d7
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-10-29 21:55:54 +0700

    Изменил модель Review, произвел миграцию

--- Models/Core data/Review.cs
+++ Models/Core data/Review.cs
@@ -1,19 +1,33 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace MovieCatalogAPI.Models
 {
     public class Review
     {
         public Guid Id { get; set; }
-        [Required]
+        [Required, JsonIgnore]
         public Movie Movie { get; set; }
-        public string? ReviewText { get; set; }
+        [Required]
+        public string ReviewText { get; set; }
+        [Range(0, 10)]
         public int Rating { get; set; }
         public bool IsAnonymous { get; set; }
         public DateTime CreateDateTime { get; set; }
-        [Required]
-        public User Author { get; set; }
+        [Required, JsonIgnore]
+        public User AuthorData { get; set; }
+        [NotMapped]
+        public UserShort Author { get; set; }
+        public Review()
+        {
+            Author = new UserShort
+            {
+                UserID = AuthorData.Id,
+                Avatar = AuthorData.AvatarLink,
+                UserName = AuthorData.UserName
+            };
+        }
 
         public ReviewShort ToShort()
         {
---
+++ Models/DTO/UserShort.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCatalogAPI.Models
+{
+    public class UserShort
+    {
+        public Guid UserID { get; set; }
+        public string? UserName { get; set; }
+        public string? Avatar { get; set; }
+    }
+}

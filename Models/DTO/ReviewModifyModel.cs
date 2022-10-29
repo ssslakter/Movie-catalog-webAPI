44896e0958b22a9994a20499e609658b4dd0e90d
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-10-29 21:55:54 +0700

    Изменил модель Review, произвел миграцию

---
+++ Models/DTO/ReviewModifyModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCatalogAPI.Models
+{
+    public class ReviewModifyModel
+    {
+        [Required]
+        public string ReviewText { get; set; }
+        [Range(0, 10)]
+        public int Rating { get; set; }
+        public bool IsAnonymous { get; set; }
+
+        public ReviewModifyModel(string reviewText)
+        {
+            ReviewText = reviewText;
+        }
+    }
+}

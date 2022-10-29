ee45f8adb1878332ea0950fb1e0e0134c0b9852e
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-10-29 23:08:01 +0700

    FIX Исправил отображение отзывов в деталях фильма

--- Models/Core data/Review.cs
+++ Models/Core data/Review.cs
@@ -19,21 +19,22 @@ namespace MovieCatalogAPI.Models
         public User AuthorData { get; set; }
         [NotMapped]
         public UserShort Author { get; set; }
-        public Review()
-        {
-            Author = new UserShort
-            {
-                UserID = AuthorData.Id,
-                Avatar = AuthorData.AvatarLink,
-                UserName = AuthorData.UserName
-            };
-        }
 
         public ReviewShort ToShort()
         {
             return new ReviewShort(Rating)
             {
                 Id = Id
+            };
+        }
+
+        public void AddAuthorShort()
+        {
+            Author = new UserShort
+            {
+                UserID = AuthorData.Id,
+                UserName = AuthorData.UserName,
+                Avatar = AuthorData.AvatarLink
             };
         }
     }

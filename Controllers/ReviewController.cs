33fbf0c9d382f78be04fb3904cfeb08860ce8bc8
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-10-29 21:25:07 +0700

    Создал контроллер с заголовками запросов

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieCatalogAPI.Controllers
+{
+    [Route("api/review")]
+    [ApiController]
+    public class ReviewController : Controller
+    {
+        [HttpPost("{movieId}/review/add")]
+        public IActionResult AddReview([FromRoute] Guid movieId)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut("{movieId}/review/edit")]
+        public IActionResult EditReview([FromRoute] Guid movieId)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{movieId}/review/delete")]
+        public IActionResult DeleteReview([FromRoute] Guid movieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
